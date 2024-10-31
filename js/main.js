fadf68fa9491e3868ec66994be6840075ad24d21
Samandar <samandartursunov217@gmail.com>
2024-10-31 20:54:25 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// burger
+const headerBurger = document.querySelector(".header-burger");
+const headerList = document.querySelector(".header-list")
+
+
+headerBurger.addEventListener("click",() => {
+  headerBurger.classList.toggle("active");
+  headerList.classList.toggle("active");
+  document.body.classList.toggle("active");
+})
